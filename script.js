66dba77aa844e1e66378d59eb1847c63a366a230
Corentin41 <101725256+Corentin41@users.noreply.github.com>
2025-11-23 14:41:14 +0100

    FIX age + desc profile

--- script.js
+++ script.js
@@ -11,20 +11,18 @@ btnBurger.addEventListener("click", function () {
     ulMobile.style.display = "block";
     btnBurger.classList.add("menuBurgerOpen");
     restart = true;
-  }
-
-  else {
+  } else {
     navMobile.style.width = "0vw";
     ulMobile.style.display = "none";
     btnBurger.classList.remove("menuBurgerOpen");
     restart = false;
   }
-})
+});
 
 // Click sur un élément du menu le fait disparaître
 let menu = document.querySelectorAll(".menu");
 
-menu.forEach(item => {
+menu.forEach((item) => {
   item.addEventListener("click", () => {
     navMobile.style.width = "0vw";
     ulMobile.style.display = "none";
@@ -40,17 +38,19 @@ let date = new Date();
 // Dynamic age
 let ageDisplay = document.querySelector(".age");
 if (ageDisplay != null) {
+  ageDisplay.innerHTML = getAge();
+}
+
+function getAge() {
   let myBirthYear = 2002;
   let currentYear = date.getFullYear();
   let currentDay = date.getDate();
   let currentMonth = date.getMonth() + 1; // + 1 because index between 0 and 11
-  if (currentDay >= 25 && currentMonth >= 2) {
-    ageDisplay.innerHTML = currentYear - myBirthYear;
-  } else {
-    ageDisplay.innerHTML = currentYear - myBirthYear - 1;
+  if ((currentMonth == 2 && currentDay >= 25) || currentMonth >= 2) {
+    return currentYear - myBirthYear;
   }
+  return currentYear - myBirthYear - 1;
 }
-
 
 // Date footer
 let footerYearDisplay = document.querySelector(".year");
@@ -58,11 +58,11 @@ footerYearDisplay.innerHTML = date.getFullYear();
 
 /*----- ANIMATIONS -----*/
 
-const options = { root: null, rootMargin: '0px', threshold: 1 };
+const options = { root: null, rootMargin: "0px", threshold: 1 };
 
 function startAnimation(animation_name) {
   const startAnimation = (entries) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       entry.target.classList.toggle(animation_name, entry.isIntersecting);
     });
   };
@@ -70,43 +70,57 @@ function startAnimation(animation_name) {
 }
 
 // Slide from right animation call
-const elementsRight = document.querySelectorAll('.slideElementFromRight');
-elementsRight.forEach(element => {
-  new IntersectionObserver(startAnimation("slide-in-from-right")).observe(element, options);
+const elementsRight = document.querySelectorAll(".slideElementFromRight");
+elementsRight.forEach((element) => {
+  new IntersectionObserver(startAnimation("slide-in-from-right")).observe(
+    element,
+    options
+  );
 });
 
 // Slide from left animation call
-const elementsLeft = document.querySelectorAll('.slideElementFromLeft');
-elementsLeft.forEach(element => {
-  new IntersectionObserver(startAnimation("slide-in-from-left")).observe(element, options);
+const elementsLeft = document.querySelectorAll(".slideElementFromLeft");
+elementsLeft.forEach((element) => {
+  new IntersectionObserver(startAnimation("slide-in-from-left")).observe(
+    element,
+    options
+  );
 });
 
 // Projects pop animation call
-const elementsPop = document.querySelectorAll('.popElement');
-elementsPop.forEach(elPop => {
-  new IntersectionObserver(startAnimation("pop_animation")).observe(elPop, options);
+const elementsPop = document.querySelectorAll(".popElement");
+elementsPop.forEach((elPop) => {
+  new IntersectionObserver(startAnimation("pop_animation")).observe(
+    elPop,
+    options
+  );
 });
 
 // Skills pop animation call
-const elementsSkillsPop = document.querySelectorAll('.skill_pop');
+const elementsSkillsPop = document.querySelectorAll(".skill_pop");
 popAnimationDelay();
-elementsSkillsPop.forEach(element => {
-  new IntersectionObserver(startAnimation("skill_pop_animation")).observe(element, options);
+elementsSkillsPop.forEach((element) => {
+  new IntersectionObserver(startAnimation("skill_pop_animation")).observe(
+    element,
+    options
+  );
 });
 
 /*----- POP ANIMATION DELAY -----*/
 
 // add animation delay for each skill_box
 function popAnimationDelay() {
-  var skills = document.querySelectorAll('.skills');
-  var tools = document.querySelectorAll('.tools');
+  var skills = document.querySelectorAll(".skills");
+  var tools = document.querySelectorAll(".tools");
 
   // animation delay for skill/language box
-  skills.forEach(box => {
+  skills.forEach((box) => {
     // increase the delay
     increaseDelay();
     // add the new delay to the "box" element
-    let pop_delay = getComputedStyle(document.documentElement).getPropertyValue('--pop-delay');
+    let pop_delay = getComputedStyle(document.documentElement).getPropertyValue(
+      "--pop-delay"
+    );
     box.style.animationDelay = pop_delay;
   });
 
@@ -114,39 +128,43 @@ function popAnimationDelay() {
   resetDelay();
 
   // animation delay for tool box
-  tools.forEach(box => {
+  tools.forEach((box) => {
     // increase the delay
     increaseDelay();
     // add the new delay to the "box" element
-    let pop_delay = getComputedStyle(document.documentElement).getPropertyValue('--pop-delay');
+    let pop_delay = getComputedStyle(document.documentElement).getPropertyValue(
+      "--pop-delay"
+    );
     box.style.animationDelay = pop_delay;
   });
 }
 
 // reset the delay var in :root element in css file
 function resetDelay() {
-  const root_theme = document.querySelector(':root');
-  root_theme.style.setProperty('--pop-delay', "0.0s");
+  const root_theme = document.querySelector(":root");
+  root_theme.style.setProperty("--pop-delay", "0.0s");
 }
 
 // increase delay between each skill box pop
 function increaseDelay(pop_delay) {
   // get the pop delay and transform from string to int
-  pop_delay = getComputedStyle(document.documentElement).getPropertyValue('--pop-delay');
+  pop_delay = getComputedStyle(document.documentElement).getPropertyValue(
+    "--pop-delay"
+  );
   let delay = pop_delay.replace("s", "");
   // increase delay with 0.1s for each skill box
-  delay = parseFloat(delay) + parseFloat('0.1');
+  delay = parseFloat(delay) + parseFloat("0.1");
   // set the new var in the css
-  const root_theme = document.querySelector(':root');
-  root_theme.style.setProperty('--pop-delay', delay + "s");
+  const root_theme = document.querySelector(":root");
+  root_theme.style.setProperty("--pop-delay", delay + "s");
 }
 
 /*----- SKILLS SELECT TOOGLE -----*/
 
-let select_languages = document.querySelector('#select_languages');
-let select_tools = document.querySelector('#select_tools');
-let skills_languages = document.querySelector('#skills_languages');
-let skills_tools = document.querySelector('#skills_tools');
+let select_languages = document.querySelector("#select_languages");
+let select_tools = document.querySelector("#select_tools");
+let skills_languages = document.querySelector("#skills_languages");
+let skills_tools = document.querySelector("#skills_tools");
 
 // Par défaut : afficher les langages et cacher les outils
 select_languages.classList.toggle("skills_toggle_select");
@@ -193,7 +211,7 @@ display = 1;
 display_btn();
 
 // retirer de l'affichage les projets
-hidden_projects.forEach(project => {
+hidden_projects.forEach((project) => {
   project.style.display = "none";
 });
 
@@ -201,19 +219,19 @@ hidden_projects.forEach(project => {
 view_more_project.addEventListener("click", () => {
   display = 0;
   display_btn();
-  hidden_projects.forEach(project => {
+  hidden_projects.forEach((project) => {
     project.style.display = "flex";
-  })
+  });
 });
 
 // voir moins de projets
 view_less_project.addEventListener("click", () => {
   display = 1;
   display_btn();
-  hidden_projects.forEach(project => {
+  hidden_projects.forEach((project) => {
     project.style.display = "none";
-  })
-})
+  });
+});
 
 // boutons pour afficher "voir plus / voir moins"
 function display_btn() {
